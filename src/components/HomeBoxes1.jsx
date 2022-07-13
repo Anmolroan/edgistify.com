079ad32f502deb269f6152b013d4f344b7d7a659
Anmolroan <anmol.kumar@masaischool.com>
2022-07-13 21:17:34 +0530

    end for the day

---
+++ src/components/HomeBoxes1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomeBoxes1() {
+  return (
+    <div>HomeBoxes1</div>
+  )
+}
+
+export default HomeBoxes1
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
 </div>
 <div className="absolute mt-80 text-white pt-16 text-4xl ml-72 pl-8 textOnvid">DARK STORE & WAREHOUSING NETWORK</div>
 <div className="absolute mt-96 text-white pt-20 text-2xl ml-72 pl-20  textOnvid">10 min to 2 hrs delivery & next day delivery for your brand</div>
-<button className="absolute mt-96  text-white  text-xl ml-96  my-auto bg-white p-1 rounded-md text-teal-500 textOnvid">Get in touch</button>
+<button className="absolute mt-96  text-white  text-xl ml-96  my-auto bg-white p-1 rounded-md text-teal-500 getinTouch">Get in touch</button>
 <video className='videoTag  h-5/5 w-full' autoPlay loop muted>
     <source src={sample} type='video/mp4' />
    
